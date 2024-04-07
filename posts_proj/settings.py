e322dc4b1e6ee5812639ea0a59b6dcea76ad86be
Yifang Zhang <yzhang8925@conestogac.on.ca>
2024-04-06 20:00:16 -0400

    create models: post/profiles

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'profiles',
-    'crispy forms',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,12 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS={
+STATICFILES_DIRS=[
+
     BASE_DIR/ 'static',
     BASE_DIR / 'posts' /'static',
     BASE_DIR / 'profiles' /'static',
+]
+    
 
-}
+
 
 
 MEDIA_URL = '/media/'
